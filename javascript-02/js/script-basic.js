d619f408a3d928b8d49e44dbe0cc0eb41f3a50c9
KosukeJimbo <jimbokosuke.hal@gmail.com>
2022-10-31 17:36:10 +0900

    [Update]getElementまで

--- javascript-02/js/script-basic.js
+++ javascript-02/js/script-basic.js
@@ -12,19 +12,40 @@ document.write('<h2>' + userName + '</h2>');
 document.write('<img src="img/image1.png" alt="1">');
 
 // 6. リンクの追加
-document.write('<a href="https://www.google.com/">Google</a>');
+document.write('<a href="https://www.google.com/">Googleへ</a>');
 
 // ②プロパティの制御方法
 
 // 1. ドキュメントの背景色
-document.bgColor = 'olive'; // background color
+document.bgColor = '#5ababa'; // background color
 document.fgColor = 'white'; // foreground color
 
 // ③document以外のオブジェクト
 
 // 1. アラート
-window.alert('Hello JS!');
+// window.alert('Hello JS!');
 
 // 2. プロンプト
-var name = window.prompt('what is your name?', 'Enter your name');
-document.write('<br>名前: '+ name);
+// window.prompt('what is your name?', 'Enter your name');
+
+
+// 4. 変数の活用
+// 変数の種類(宣言)
+// その1： 格納する値が変化する(通常の変数) let 変数名 = 初期値;
+// その2： 格納する値が変化しない(定数) const 変数名 = 初期値;
+
+let secondUserName = "春太郎";
+document.write('<h2>' + secondUserName + '</h2>');
+
+let inputBgCol = window.prompt('背景色を入力してください', '背景色');
+document.bgColor = inputBgCol;
+
+let inputFgCol = window.prompt('文字色を入力してください', '文字色');
+document.fgColor = inputFgCol;
+
+
+// h1の文字色をコントロールする
+document.getElementsByTagName('h1').item(0).style.color = 'red';
+
+// DOM(DocumentObjectModel)
+// 読み込まれた位置でjsファイルは実行されるされる
