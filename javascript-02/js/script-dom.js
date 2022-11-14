e28c8c6104ca81eb65b796349b6622ed5da6e9df
神保 恒介 <81564943+kosukej@users.noreply.github.com>
2022-11-14 20:02:50 +0900

    ✨ DOM基礎第二回の進捗

--- javascript-02/js/script-dom.js
+++ javascript-02/js/script-dom.js
@@ -23,5 +23,28 @@ div1.style.width = '200px'; // div要素の横幅を変更
 const idSample = document.getElementById('sample');
 idSample.style.color = 'red';
 
-// (2)srcを変更する
-const sam1 = document.getElementById('sam1').style.src = 'img/image4.png'
+// ⭐️class名でHTML要素を取得
+document.getElementsByClassName('sampleP').item(0).style.color = 'blue';
+
+// (2)画像を変更する
+const sam1 = document.getElementById('sam1');
+sam1.src = 'img/image1.png';
+
+
+//③イベント(クリック)を使ってみる！
+
+//クリックごとに画像が変わる
+let photoCount = 1;
+
+const btn = document.getElementsByTagName('input').item(0);
+btn.addEventListener('click', () => {
+    // let randomNum = Math.floor(Math.random()*4) + 1;
+    // sam1.src = 'img/image' + randomNum + '.png';
+    if (photoCount === 4) {
+        photoCount = 1;
+    } else {
+        photoCount++;
+    }
+    document.getElementsByClassName('countP').item(0).textContent = photoCount + '枚目';
+    sam1.src = 'img/image' + photoCount + '.png';
+});
